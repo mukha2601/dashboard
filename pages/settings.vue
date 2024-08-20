abd5016749ef5dc0bb346f8643fd5983a344d335
muhammaddin <muhammaddin.xoliqov1998@gmail.com>
2024-08-21 03:33:07 +0500

    added fetch

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,4 +1,27 @@
 <script setup>
+const people = ref([]);
+
+onMounted(() => {
+  fetch("https://autoapi.dezinfeksiyatashkent.uz/api/categories")
+    .then((response) => response.json())
+    .then((items) => {
+      console.log(items.data);
+
+      items?.data?.map((item) => {
+        people.value.push({
+          id: item.id,
+          name: item.name_en,
+          title: item.name_ru,
+          email: item.name_ru,
+          role: item.name_ru,
+          image: `https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/${item.image_src}`,
+        });
+      });
+    });
+});
+
+console.log(people.value);
+
 const columns = [
   {
     key: "name",
@@ -9,60 +32,11 @@ const columns = [
     label: "Title",
   },
   {
-    key: "email",
-    label: "Email",
-  },
-  {
-    key: "role",
-    label: "Role",
+    key: "image",
+    label: "Image",
   },
   {
     key: "actions",
-  },
-];
-
-const people = [
-  {
-    id: 1,
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  {
-    id: 2,
-    name: "Courtney Henry",
-    title: "Designer",
-    email: "courtney.henry@example.com",
-    role: "Admin",
-  },
-  {
-    id: 3,
-    name: "Tom Cook",
-    title: "Director of Product",
-    email: "tom.cook@example.com",
-    role: "Member",
-  },
-  {
-    id: 4,
-    name: "Whitney Francis",
-    title: "Copywriter",
-    email: "whitney.francis@example.com",
-    role: "Admin",
-  },
-  {
-    id: 5,
-    name: "Leonard Krasner",
-    title: "Senior Designer",
-    email: "leonard.krasner@example.com",
-    role: "Owner",
-  },
-  {
-    id: 6,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
   },
 ];
 
@@ -85,16 +59,18 @@ const page = ref(1);
 const pageCount = 5;
 
 const rows = computed(() => {
-  return people.slice((page.value - 1) * pageCount, page.value * pageCount);
+  return people.value.slice(
+    (page.value - 1) * pageCount,
+    page.value * pageCount
+  );
 });
 </script>
 
 <template>
   <UTable :rows="rows" :columns="columns">
-    <template #name-data="{ row }">
-      <span>{{ row.name }}</span>
+    <template #image-data="{ row }">
+      <img :src="row.image" alt="Category Image" class="h-24 w-max object-cover" />
     </template>
-
     <template #actions-data="{ row }">
       <UDropdown :items="items(row)">
         <UButton
@@ -108,6 +84,7 @@ const rows = computed(() => {
   <div
     class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
   >
+    <UButton label="Add categories" class="me-4" />
     <UPagination
       v-model="page"
       :page-count="pageCount"
