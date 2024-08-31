84c594ab4bc7b3ce7a8d918cfeb33853fa0abe39
muhammaddin <muhammaddin.xoliqov1998@gmail.com>
2024-09-01 02:45:55 +0500

    sidebarga logo qoshildi

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="w-1/4 bg-[#191A19] p-4 flex flex-col  justify-center rounded-md">
-    <UVerticalNavigation :links="links">
-      <template #default="{ link }">
-        <span class="group-hover:text-primary relative ms-2 px-6 py-5">{{
-          link.label
-        }}</span>
-      </template>
-    </UVerticalNavigation>
+  <div class="w-1/4 bg-[#191A19] p-4 flex flex-col rounded-md relative">
+    <div
+      class="logo flex gap-4 items-center justify-start bg-[#292b29] w-full p-2 rounded-md"
+    >
+      <NuxtLink to="/">
+        <NuxtImg src="../public/logo.ico" width="35" />
+      </NuxtLink>
+      <p>Dashboard</p>
+    </div>
+    <div class="flex flex-col justify-center h-full">
+      <UVerticalNavigation :links="links">
+        <template #default="{ link }">
+          <span class="group-hover:text-primary relative ms-2 px-6 py-5">{{
+            link.label
+          }}</span>
+        </template>
+      </UVerticalNavigation>
+    </div>
   </div>
 </template>
 
